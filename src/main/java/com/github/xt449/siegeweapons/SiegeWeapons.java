ddb2047bc4289d01134876b16628725ea956016e
xt449 <alpha.xt449.omega@gmail.com>
2021-02-26 19:55:45 -0500

    Initial commit

---
+++ src/main/java/com/github/xt449/siegeweapons/SiegeWeapons.java
@@ -0,0 +1,17 @@
+package com.github.xt449.siegeweapons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SiegeWeapons extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
